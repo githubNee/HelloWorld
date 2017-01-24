75ffe84c62f9385ddd6214dce7e5287909b58f0d
Nee <183540581@qq.com>
2017-01-24 14:04:12 +0800

    chat api

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var app = express();
 
 var path = require('path');
 var request = require('request');
+var bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, 'public')));
 var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
 server.listen(port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.get('/', function(req,res,next){
     res.sendFile('index.html')
@@ -15,3 +19,8 @@ app.get('/', function(req,res,next){
 
 var chat = require('./routes/chat');
 app.use('/api/chat',chat);
+
+
+app.use((req,res,next)=>{
+    res.send("404 not found");
+})
--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 router.route('/')
-.get((req,res,next)=>{
-    res.send("let's chat");
+.post(function(req,res,next){
+    console.log(req.body);
+    if(req.body['chat'] == null) {
+        res.sendStatus(400);
+    } else {
+        var chat = req.body['chat'];
+
+        var reqBody = {};
+        reqBody.key = '35618a945770417a86bf7e8179283680';
+        reqBody.info = chat;
+        reqBody.userid = 0;
+        var opts = {
+            url: "http://www.tuling123.com/openapi/api",
+            method : 'POST',
+            headers: {
+                'User-Agent': 'request',
+                'content-type': 'application/json'
+            },
+        };
+        opts.body = JSON.stringify(reqBody);
+        request(opts, function (error, response, body) {
+            var data = {};
+            if (!error) {
+                data = JSON.parse(body);
+                res.send(data);
+            } else 
+                res.sendStatus(500);
+        });
+    }
 })
+
+
 
 module.exports = router;
