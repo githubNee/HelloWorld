2adea61b2c2bf75f92aeae9af3c6b6e9f4f6a417
Nee <183540581@qq.com>
2017-01-27 12:23:10 +0800

    tty.js config

--- app.js
+++ app.js
@@ -23,29 +23,6 @@ var chat = require('./routes/chat');
 app.use('/api/chat',chat); 
 app.use('/api/chat',chat);
 
-var io = require('socket.io')(server);
-io.on('connection', function(socket){
-// Create terminal
-	var term = pty.spawn('ssh', [], {
-	   name: 'xterm-color',
-	   cols: 80,
-	   rows: 30,
-	   cwd: process.env.HOME,
-	   env: process.env
-	});
-	// Listen on the terminal for output and send it to the client
-	term.on('data', function(data){
-	   socket.emit('output', data);
-	});
-	// Listen on the client and send any input to the terminal
-	socket.on('input', function(data){
-	   term.write(data);
-	});
-	// When socket disconnects, destroy the terminal
-	socket.on("disconnect", function(){
-	   term.destroy();
-	});
-});
 app.get('/shell',function(req,res,next){
 	res.sendFile('public/html/shell.html', { root: __dirname })
 });
@@ -59,9 +36,7 @@ app.use((req,res,next)=>{
 var tty = require('tty.js');
 
 var app2 = tty.createServer({
-  shell: 'ssh',
-  // hostname: '115.159.55.131',
-
+  shell: 'bash',
   users: {
     nee: 'nee960610',
     wang: 'wang961125'
